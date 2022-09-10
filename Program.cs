4295ccdbc65d8d862d18f9efc4f02009221a6826
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-09-09 22:50:33 -0300

    e a parte final do nome ninja

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("DIGITE SEU NOME:");
+string nomedigitado = Console.ReadLine()!;
+ Console.ForegroundColor = ConsoleColor.Blue;
+ Console.ResetColor();
+string nomeninja = nomedigitado 
+.ToLower()    
+.Replace("A" ,  "ka" )
+.Replace("B",  "zu"  )
+.Replace("C" , "mi ")
+.Replace("D" , "te")
+.Replace("E" , "ku")
+.Replace("F" , "lu")
+.Replace("G" , "ji")
+.Replace("H" , "ri")
+.Replace("I" , "ki")
+.Replace("J" , "zus")
+.Replace("K" , "me")
+.Replace("L" , "ta")
+.Replace("M" , "rin")
+.Replace("N" , "to")
+.Replace("O" , "mo")
+.Replace("P" , "no")
+.Replace("Q" , "ke")
+.Replace("R" , "shi")
+.Replace("S" , "ari")
+.Replace("T" , "chi")
+.Replace("U" , "do")
+.Replace("V" , "ru")
+.Replace("W" , "mei")
+.Replace("X" , "na")
+.Replace("Y" ,  "fu")
+.Replace("Z" ,  "zi");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine($"Seu nome ninja é:{nomeninja.Substring(0,1).ToUpper()}{nomeninja.Substring(1).ToLower()}");
+Console.ResetColor();
